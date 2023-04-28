3fff542ed96c786cb6eaf1d825a40f81664aff1f
Milena <mi_lky@mail.ru>
2023-04-28 18:10:02 +0300

    create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+window.addEventListener('load', () => {
+  React.render(<Header />, document.getElementById(reat_root));
+});
