407ec8bce42d83841537da3c872a94d3184e70a5
Milena <mi_lky@mail.ru>
2023-04-28 18:48:12 +0300

    feat: set webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const NODE_ENV = process.env.NODE_ENV;
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const IS_DEV = NODE_ENV === 'development';
 
 module.exports = {
   resolve: {
@@ -23,4 +24,9 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
   ],
+  devServer: {
+    port: 3000,
+    open: true, //автоматически открывать при запуске localhost:3000
+    hot: IS_DEV, //автоматически перезапускать сервер при наличии изменений
+  },
 };
